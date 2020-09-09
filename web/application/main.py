cc7cef5253a2e35e8624c6ec1d3f6a6c143e70e4
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-09-09 12:19:43 +0300

    Add pagination

--- web/application/main.py
+++ web/application/main.py
@@ -20,18 +20,25 @@ def hello():
     )
 
 
-def search_joke(keywords):
-    return list(jokes.find({"$text": {"$search": keywords}}))
+def search_joke(keywords, page):
+    page_size = 10
+    offset = page * page_size
+    res = list(jokes.find({"$text": {"$search": keywords}}).skip(offset).limit(page_size + 1))
+    return res[:-1], len(res) > page_size
 
 
 @app.route('/get_joke', methods=['GET'])
 def get_joke():
     joke_keywords = request.args.get('keywords')
-    found_jokes = search_joke(joke_keywords)
+    page = int(request.args.get('page', 0))
+    found_jokes, next_page = search_joke(joke_keywords, page)
     return render_template(
         'main.html',
-        jokes=found_jokes[0:-1],
+        keywords=joke_keywords,
+        jokes=found_jokes,
         no_searches=False,
+        page=page,
+        next_page=next_page
     )
 
 
